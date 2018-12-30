3d459b10e9ce7a8c3dc2c273825bbc8ff8deccc4
Carlos Araoz <caraoz@users.noreply.github.com>
2018-12-30 10:11:55 -0500

    minimal install

---
+++ ubuntu.18.04-minimal.sh
@@ -0,0 +1,10 @@
+## set up basic packages for minimal install
+apt update;
+apt install tmux pv nload htop git iotop curl ntpdate;
+
+## sync time to prevent future update errors
+ntpdate 1.ro.pool.ntp.org;
+
+## install python modules
+apt install python3-pip;
+pip3 install youtube-dl BeautifulSoup4 schedule;
