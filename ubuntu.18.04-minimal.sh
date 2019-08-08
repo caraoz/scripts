2512c0216ddf7a01b93510b22221120e13bf6c24
Carlos Araoz <caraoz@users.noreply.github.com>
2019-08-08 07:34:28 -0400

    new pkgs

--- ubuntu.18.04-minimal.sh
+++ ubuntu.18.04-minimal.sh
@@ -1,6 +1,6 @@
 ## set up basic packages for minimal install
 apt update;
-apt install tmux pv nload htop git iotop curl ntpdate;
+apt install tmux pv nload htop git iotop curl ntpdate nethogs nmap ;
 
 ## sync time to prevent future update errors
 ntpdate 1.ro.pool.ntp.org;
